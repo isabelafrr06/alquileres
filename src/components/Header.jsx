1f726d99b08d038a62928b00b58b847c9e7b3d0e
Isabela <isabelafrr06@hotmail.com>
2023-08-16 15:54:12 -0600

    Header profile config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,69 @@
-import { useLocation, useNavigate } from "react-router-dom"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Header() {
-    const location = useLocation()
-    //console.log(location.pathname)
-    const navigate = useNavigate()
-    function pathMathRoute(route) {
-        if (route === location.pathname) {
-            return true
-        }
-    }
-  return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
-        <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
-            <div>
-                <img src="" alt="logo" className="h-5 cursor-pointer" onClick={()=>navigate("/")} />
-            </div>
-            <div>
-                <ul className="flex space-x-5">
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 ${pathMathRoute("/") && "text-black border-b-4 border-b-red-500"}`} onClick={()=>navigate("/")}>Inicio</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 ${pathMathRoute("/offers") && "text-black border-b-4 border-b-red-500"}`} onClick={()=>navigate("/offers")}>Ofertas</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 ${pathMathRoute("/sign-in") && "text-black border-b-4 border-b-red-500"}`} onClick={()=>navigate("/sign-in")}>Iniciar Sesión</li>
-                </ul>
-            </div>
-        </header>
-    </div>
-  )
+	const [pageState, setPageState] = useState("Iniciar sesión");
+	const location = useLocation();
+	//console.log(location.pathname)
+	const navigate = useNavigate();
+	const auth = getAuth();
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setPageState("Perfil");
+			} else {
+				setPageState("Iniciar sesión");
+			}
+		});
+	}, [auth]);
+	function pathMatchRoute(route) {
+		if (route === location.pathname) {
+			return true;
+		}
+	}
+	return (
+		<div className="bg-white border-b shadow-sm sticky top-0 z-50">
+			<header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+				<div>
+					<img
+						src=""
+						alt="logo"
+						className="h-5 cursor-pointer"
+						onClick={() => navigate("/")}
+					/>
+				</div>
+				<div>
+					<ul className="flex space-x-5">
+						<li
+							className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 ${
+								pathMatchRoute("/") && "text-black border-b-4 border-b-red-500"
+							}`}
+							onClick={() => navigate("/")}
+						>
+							Inicio
+						</li>
+						<li
+							className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 ${
+								pathMatchRoute("/offers") &&
+								"text-black border-b-4 border-b-red-500"
+							}`}
+							onClick={() => navigate("/offers")}
+						>
+							Ofertas
+						</li>
+						<li
+							className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 ${
+								(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+								"text-black border-b-4 border-b-red-500"
+							}`}
+							onClick={() => navigate("/profile")}
+						>
+							{pageState}
+						</li>
+					</ul>
+				</div>
+			</header>
+		</div>
+	);
 }
